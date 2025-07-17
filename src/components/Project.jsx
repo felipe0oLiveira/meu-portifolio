356ed743b676cf666bcb4fec39b9f52519226a1e
Felipe Oliveira <lippe637@gmail.com>
2025-07-17 20:04:50 -0300

    refactor(project): melhorias na exibição dos projetos

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import ProjectDetails from "./ProjectDetails";
+import { useTranslation } from 'react-i18next';
 
 const Project = ({
-  title,
-  description,
-  subDescription,
+  i18nKey,
   href,
   image,
   tags,
   setPreview,
 }) => {
   const [isHidden, setIsHidden] = useState(false);
+  const { t, i18n } = useTranslation();
   return (
     <>
       <div
@@ -19,7 +19,7 @@ const Project = ({
         onMouseLeave={() => setPreview(null)}
       >
         <div>
-          <p className="text-2xl">{title}</p>
+          <p className="text-2xl">{t(`projects.${i18nKey}.title`)}</p>
           <div className="flex gap-5 mt-2 text-sand">
             {tags.map((tag) => (
               <span key={tag.id}>{tag.name}</span>
@@ -30,16 +30,16 @@ const Project = ({
           onClick={() => setIsHidden(true)}
           className="flex items-center gap-1 cursor-pointer hover-animation"
         >
-          Read More
+          {t('projects.readMore', 'Read More')}
           <img src="assets/arrow-right.svg" className="w-5" />
         </button>
       </div>
       <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full" />
       {isHidden && (
         <ProjectDetails
-          title={title}
-          description={description}
-          subDescription={subDescription}
+          title={t(`projects.${i18nKey}.title`)}
+          description={t(`projects.${i18nKey}.description`)}
+          subDescription={t(`projects.${i18nKey}.subDescription`, { returnObjects: true })}
           image={image}
           tags={tags}
           href={href}
